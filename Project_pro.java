beedff5caf9a10f6f83adeb3e6ec36669d29a8d3
areschen <areschentw@outlook.com>
2020-06-05 21:45:56 +0800

    605

--- Project_pro.java
+++ Project_pro.java
@@ -3,23 +3,29 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 import java.text.DecimalFormat;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Vector;
 
 public class Project_pro extends JFrame implements ActionListener{
-	static int namessss;
+	static int drop_nuber=6; //control drop number
     static Project_pro main_app; //JFrame
     static int move; // determine left or right
     static int move_x=280; //character location x
     static int move_y=525; //character location y
-    static int[] dropx = {10, 10, 10, 10, 10,10}; //drop location x
-    static int[] dropy = {10, 10, 10, 10, 10,10}; //drop location y
-    static int[] dropSize = {50, 50, 50, 50, 50,50}; //drop Size
+    static int []dropx=new int[20];//drop location x
+    static int []dropy=new int[20]; //drop location y
+    static int []dropSize=new int[20]; //drop Size
     static JLabel name_label; //uers name
+    static JLabel time_label; //time
     static boolean clock_con=false; //check wether timer control
     static boolean exits=false; //check wether exit
     private UserPanel userPane; //JPanel
     private Timer timer; //timer
     static double times=0; //record time
-    static Image img_turtle,img_mushroom,img_ghost1,img_ghost2,img,b1; //set Image
+    static Image img_turtle,img_mushroom,img_ghost1,img_ghost2,img,backgrounds; //set Image
+    static Vector vec_loading = new Vector();
 
     /* JPanel constructer */ 
     class UserPanel extends JPanel{
@@ -55,18 +61,18 @@ public class Project_pro extends JFrame implements ActionListener{
             }
             else //if is timer event
                 clock_con=false;
-            g.drawImage(b1,0 , 0, 600,600,this); //Draw turtle
-            g.drawImage(img_turtle,dropx[5] , dropy[5], dropSize[5],dropSize[5],this); //Draw turtle
-            g.drawImage(img_mushroom,dropx[0] , dropy[0], dropSize[0],dropSize[0],this); //Draw mushroom1
-            g.drawImage(img_mushroom,dropx[1] , dropy[1], dropSize[1],dropSize[1],this); //Draw mushroom2
-            g.drawImage(img_mushroom,dropx[2] , dropy[2], dropSize[2],dropSize[2],this); //Draw mushroom3
-            g.drawImage(img_mushroom,dropx[3] , dropy[3], dropSize[3],dropSize[3],this); //Draw mushroom4
-            g.drawImage(img_mushroom,dropx[4] , dropy[4], dropSize[4],dropSize[4],this); //Draw mushroom5
+            g.drawImage(backgrounds,0 , 0, 600,600,this); //Draw background
+            for(int i=0;i<=drop_nuber;i++)
+                if(i==5 || i==10 || i==19)
+                    g.drawImage(img_turtle,dropx[i] , dropy[i], dropSize[i],dropSize[i],this); //Draw turtle
+            for(int i=0;i<=drop_nuber;i++)
+                if(i!=5 && i!=10 && i!=19)
+                    g.drawImage(img_mushroom,dropx[i] , dropy[i], dropSize[i],dropSize[i],this); //Draw mushroom
             g.drawImage(img,move_x , 525, 75,75,this); //Draw character
         }
         /* Paint End */
      }
-    /*JPanel constructer End*/ 
+    /*JPanel constructer End*/  
 
     /*JFrame constructer*/
     public Project_pro() { 
@@ -80,6 +86,10 @@ public class Project_pro extends JFrame implements ActionListener{
         userPane.setBorder(BorderFactory.createLineBorder(Color.red));
         c.add(userPane);
         c.add(name_label);
+        c.add(time_label);
+        
+
+
         timer.start();
 
 
@@ -106,43 +116,52 @@ public class Project_pro extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent evt) {
         clock_con=true; //set timer event
         times+=0.1;
+        DecimalFormat df = new DecimalFormat("##.0");
+        times = Double.parseDouble(df.format(times));
+        time_label.setText(Double.toString(times)+"s");
+        if(times%5==0 && drop_nuber<19){
+            drop_nuber++;
+            if(times>=30)
+                drop_nuber++;
+            if(drop_nuber>19)
+                drop_nuber=19;
+            System.out.println("add");
+            System.out.println(times);
+            System.out.println(drop_nuber);
+        }
         /* Condition1 */
-        for(int i=0;i<6;i++){
+        for(int i=0;i<=drop_nuber;i++){
             if(dropy[i]+dropSize[i]-35>=move_y){
-                if(dropx[i]+dropSize[i]+10>=move_x&&dropx[i]<=move_x+70){
-                    if(i!=5){
+                if(dropx[i]+dropSize[i]>=move_x+10&&dropx[i]<=move_x+60){
+                    if(i!=5 && i!=10 && i!=19){
                         dropx[i]=(int)(Math.random()*600);
                         while(dropx[i]<=0||dropx[i]+dropSize[i]>=600)
                             dropx[i]=(int)(Math.random()*600);
                         dropy[i]=10;
                         if(dropSize[i]<=300)
                             dropSize[i]+=30;
-                        System.out.println("catch!");
                     }
                     else{
                         exits=true;
-                        System.out.println("Opps!");
                     }
                 }
             }
         }
 
         /* Condition2 */
-        for(int i=0;i<6;i++) //if down to horizon
-            if(dropy[i]+50>=600){
+        for(int i=0;i<=drop_nuber;i++) //if down to horizon
+            if(dropy[i]+dropSize[i]>=600){
                 dropx[i]=(int)(Math.random()*600);
                 while(dropx[i]<=0||dropx[i]+dropSize[i]>=600)
                     dropx[i]=(int)(Math.random()*600);
                 dropy[i]=10;
             }
         
-        for(int i=0;i<6;i++) //set different speed
+        for(int i=0;i<=drop_nuber;i++) //set different speed
             dropy[i]+=i+5;
 
         if(exits){ //Game Over
             timer.stop();
-            DecimalFormat df = new DecimalFormat("##.0");
-            times = Double.parseDouble(df.format(times));
             JOptionPane.showMessageDialog(main_app,"Hi "+name_label.getText()+", Game Over!\nYou continue: "+Double.toString(times)+" sec","Over",JOptionPane.QUESTION_MESSAGE);
             System.exit(0);
         }
@@ -157,6 +176,22 @@ public class Project_pro extends JFrame implements ActionListener{
             names="Roberson";
         JOptionPane.showMessageDialog(main_app,"Hi "+names+", Welcome to ball game!","Name Confirm",JOptionPane.QUESTION_MESSAGE);
         name_label=new JLabel(names);
+        time_label=new JLabel("0.0");
+        try {
+            File input_file = new File("filename.txt");
+            Scanner myReader = new Scanner(input_file);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                vec_loading.add(new Double(Double.parseDouble(data)));
+                System.out.println(Double.parseDouble(data));
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+        for(int i=0;i<vec_loading.size();i++)
+            System.out.println(vec_loading[i]); 
     }
     /* Input Name End */
 
@@ -167,13 +202,15 @@ public class Project_pro extends JFrame implements ActionListener{
         img_ghost2 = toolkit.getImage("ghost_right.png");
         img_turtle=toolkit.getImage("turtle.png");
         img_mushroom=toolkit.getImage("mushroom.png");
-        b1=toolkit.getImage("b1.jpg");
+        backgrounds=toolkit.getImage("background1.jpg");
         img=img_ghost1;
 
-        for(int i=0;i<6;i++) //set different init x location
+        for(int i=0;i<20;i++) //set different init x location
             dropx[i]=(int)(Math.random()*525)+15;
-        for(int i=0;i<6;i++) //set different init y location
+        for(int i=0;i<20;i++) //set different init y location
             dropy[i]=(int)(Math.random()*20)-30;
+        for(int i=0;i<20;i++) //set different init drop size
+            dropSize[i]=50;
     }
     /* Image setting End */
 
